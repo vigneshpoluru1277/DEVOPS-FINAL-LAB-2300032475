8cfbf214e7c37f9c744b9a873b5018cd37db3908
vignesh <vigneshpoluru1277@gmail.com>
2025-11-17 14:24:52 +0530

    Updated docker-compose.yml

--- reactapp/src/App.jsx
+++ reactapp/src/App.jsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from "react";
 import "./App.css";
 
 function App() {
-  const BASE_URL = "http://localhost:2000/course";
+  const BASE_URL = "http://localhost:30032/course";
 
   const [courses, setCourses] = useState([]);
   const [newCourse, setNewCourse] = useState({
